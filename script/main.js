f547a6c4fccfd979e61e7ba1494870291aab6003
ibwmahin <ibwmahin@gamil.com>
2025-06-21 23:19:03 +0600

    fast update

--- script/main.js
+++ script/main.js
@@ -1,1 +1,10 @@
+// taking the variable by the getElementById
+const editor = new DrawFlow(document.getElementsByClassName("drawflow"));
+editor.start();
+const canvas = document.getElementById("drawflow");
+const sidebar = document.getElementById("sidebar");
 
+function gatCanvasCoordinates(event) {
+  const rect = canvas.getBoundingClientRect();
+  const precanvas = canvas.querySelector(".drawflow");
+}
